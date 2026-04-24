a35e9b51b7354b1f9899b5a6391006e6fb67941a
junge8971 <junge8971@gmail.com>
2026-04-24 12:49:07 +0300

    human: update netlify.toml

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  appType: 'spa',
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
-  preview: {
-    host: '0.0.0.0'
-  },
-  server: {
-    host: '0.0.0.0'
-  }
+    base: '/',
+    appType: 'spa',
+    build: {
+        outDir: 'dist',
+        sourcemap: true,
+    },
+    preview: {
+        host: '0.0.0.0',
+    },
+    server: {
+        host: '0.0.0.0',
+    },
 });
